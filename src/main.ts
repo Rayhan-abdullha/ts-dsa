8c40dddefee829daf2bd65e70d6eb21a6589f44b
Rayhan-abdullha <captainrayhan215@gmail.com>
2026-02-16 21:12:51 +0600

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,37 @@
-enum Status {
-  Success = "success",
-  Error = "error",
-  Pending = "pending"
+// enum Status {
+//   Success = "success",
+//   Error = "error",
+//   Pending = "pending"
+// }
+
+// let orderStatus: Status = Status.Success
+
+
+
+
+// let direction: "up" | "down" | "left" | "right"
+
+// direction = "up"   // ✅
+// direction = "down" // ✅
+
+
+// type User = { name: "string" }
+// type Admin = { role: "string" }
+// type AdminUser = User & Admin & {
+//   isActive: boolean
+// }
+
+
+// function printValue<T>(value: T): T {
+//   return value
+// }
+// const res = printValue("coder")
+// const res1 = printValue({name: 'coder', age: 22})
+
+
+function getFirstItem<T>(items: T[]): T {
+  return items;
 }
 
-let orderStatus: Status = Status.Success
+const first = getFirstItem([10, 20, 30]);
+const firstName = getFirstItem(["Rayhan", "Coder"]);
