172280264f8b7523424983bcc8afd76ab482dfc2
Rayhan-abdullha <captainrayhan215@gmail.com>
2026-04-08 19:20:12 +0600

    solved a problem

--- src/phase-2/practice.ts
+++ src/phase-2/practice.ts
@@ -1,16 +1,30 @@
 
-const arr: Array<string | number> = ["coder", 124, "coder", "rayhan"]
+// const arr: Array<string | number> = ["coder", 124, "coder", "rayhan"]
 
-function filterNumber(arr: Array<string | number>) {
-    const numArr = []
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        if (typeof element !== 'number') {
-            continue
-        } else {
-            numArr.push(element)
-        }
-    }
-    return numArr
-}
-console.log(filterNumber(arr))
+// function filterNumber(arr: Array<string | number>) {
+//     const numArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
+//         if (typeof element !== 'number') {
+//             continue
+//         } else {
+//             numArr.push(element)
+//         }
+//     }
+//     return numArr
+// }
+// console.log(filterNumber(arr))
+
+
+// const filterNumber = [1, 2, "coder"].filter(e => typeof e === 'number')
+// console.log(filterNumber)
+
+// function greet(name: string, age: number = 123) {
+//     console.log(name)
+// }
+// greet("coder")
+
+// function sum(...num: number[]) {
+//     return num.reduce((acc, curr) => acc += curr ,0)
+// }
+// console.log(sum(1,2,3,4,5))
