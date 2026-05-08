146d95b7f7e1243b2b29dce972d88afbabda6c86
rayhan_abdullha <captainrayhan215@gmail.com>
2026-05-08 14:18:40 +0600

    solved problem

--- src/array/problem/basic.ts
+++ src/array/problem/basic.ts
@@ -29,3 +29,53 @@ function missingNumber(nums: number[]){
     }
 };
 missingNumber([3, 0, 1])
+
+
+const arr1 = [10, 20, 30, 40, 50]
+const insert = 50
+const position = 3
+
+function insertByPosition(arr: number[], insert: number, pos: number){
+    if (arr.length < pos){
+        console.log('invalid position provided!')
+        return
+    }
+    if (pos < 0){
+        console.log('invalid position provided!')
+        return
+    }
+    for (let i = 0; i < arr.length; i++){
+        if (i === pos){
+            arr[i] = insert
+            break
+        }
+    }
+    return arr
+}
+console.log(insertByPosition(arr1, insert, position))
+
+
+const arr12 = [10, 20, 50, 40, 50, 10, 40]
+const freq = new Map()
+function printFreqValueOnlyOne(arr: number[]){
+    for (const element of arr) {
+        if (!freq.has(element)){
+            freq.set(element, element)
+        }
+    }
+    freq.forEach(v => console.log(v))
+}
+printFreqValueOnlyOne(arr12)
+
+
+
+const arr3 = [10, 12, 20, 25, 13, 10, 9, 40, 60, 5]
+function checkIsInputExist(arr: number[], check: number){
+    for (let i = 0; i < arr.length; i++) {
+        const val = arr[i]
+        if (val === check){
+            return i
+        }
+    }
+}
+console.log(checkIsInputExist(arr3, 13))
--- src/main.ts
+++ src/main.ts
@@ -80,17 +80,3 @@ bst.insert(200)
 
 // console.log(maxFreqSum("og"));          // 2
 // console.log(maxFreqSum("successes"));       // 1
-
-const arr = [-7, 2, 3, 8, 6, 6 ,75, 38, 3, 2]
-function findSecondLargestNumber(arr: number[]){
-    let stack = []
-    let findMostMax = Math.max(...arr)
-    let secondMax = 0
-    for (const n of arr){
-        if (n > secondMax && n !== findMostMax){
-            secondMax = n
-        }
-    }
-    return secondMax
-}
-console.log(findSecondLargestNumber(arr))
